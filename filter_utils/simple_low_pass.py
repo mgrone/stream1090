2bebd2fe7310a69f09c704e625cd0b6eb9f83219
mgrone <226090022+mgrone@users.noreply.github.com>
2026-01-20 01:18:59 +0100

    Reset the margins back to something reasonable.

--- filter_utils/simple_low_pass.py
+++ filter_utils/simple_low_pass.py
@@ -1,3 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright 2025 Martin Gronemann
+#
+# This file is part of stream1090 and is licensed under the GNU General
+# Public License v3.0. See the top-level LICENSE file for details.
+#
+
+
 from scipy.signal import firwin
 
 numtaps = 15
--- filter_utils/simple_sweep.py
+++ filter_utils/simple_sweep.py
@@ -1,3 +1,10 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright 2025 Martin Gronemann
+#
+# This file is part of stream1090 and is licensed under the GNU General
+# Public License v3.0. See the top-level LICENSE file for details.
+#
+
 from scipy.signal import firwin2
 import numpy as np
 from filter_module import (
