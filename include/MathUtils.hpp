f18055da23960abe6d3c973a2be58c4e109d27b6
mgrone <226090022+mgrone@users.noreply.github.com>
2026-01-23 00:20:08 +0100

    Less recursion. Should silence now both cland and gcc.

--- include/MathUtils.hpp
+++ include/MathUtils.hpp
@@ -12,37 +12,41 @@
 #include <cmath>
 
 namespace MathUtils {
-	namespace Details {
-		// constexpr approximation of the square root
-		constexpr double constSqrtNR(double x, double curr, double prev) {
-			return (curr == prev) ? curr : constSqrtNR(x, 0.5 * (curr + x / curr), curr);
-		}
+namespace Details {
 
-		// compute all possible values for an 8-bit iq pair
-		constexpr auto fillSqrtTable() {
-			std::array<float, 65536> table{ 0 };
-			for (auto i = 0; i < 256; i++) {
-				for (auto q = 0; q < 256; q++) {
-					// make the iq bytes from the input signed and between -1.0 ... 1.0
-					const double f_i = ((double)i - 127.5) / 127.5;
-					const double f_q = ((double)q - 127.5) / 127.5;
-					const double sq = f_i * f_i + f_q * f_q;
-					// we dont care about endianness here due to symmetry
-					table[(static_cast<std::array<float, 65536>::size_type>(i) << 8) | q] = (float)constSqrtNR(sq, sq, 0.0);
-				}
-			}
-			return table;
-		}
+    constexpr double constSqrtNR(double x) {
+        double curr = x;
+        for (int i = 0; i < 10; i++) {
+            curr = 0.5 * (curr + x / curr);
+        }
+        return curr;
+    }
 
-		// the LUT used for the square root
-		inline constexpr auto iq_sqrt_table{ fillSqrtTable() };
-	}
+    constexpr auto fillSqrtTable() {
+        std::array<float, 65536> table{};
 
-	constexpr float sqrt_iq(uint16_t iq) {
-		return Details::iq_sqrt_table[iq];
-	}
+        for (int i = 0; i < 256; i++) {
+            for (int q = 0; q < 256; q++) {
 
-	constexpr float sqrt_iq(uint8_t i, uint8_t q) {
-		return sqrt_iq((i << 8) | q);
-	}
+                const double f_i = (static_cast<double>(i) - 127.5) / 127.5;
+                const double f_q = (static_cast<double>(q) - 127.5) / 127.5;
+                const double sq = f_i * f_i + f_q * f_q;
+
+                table[(i << 8) | q] = static_cast<float>(constSqrtNR(sq));
+            }
+        }
+        return table;
+    }
+
+    inline constexpr auto iq_sqrt_table = fillSqrtTable();
 }
+
+constexpr float sqrt_iq(uint16_t iq) {
+    return Details::iq_sqrt_table[iq];
+}
+
+constexpr float sqrt_iq(uint8_t i, uint8_t q) {
+    return sqrt_iq((i << 8) | q);
+}
+
+} // namespace MathUtils
