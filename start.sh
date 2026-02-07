0111f92e06421d89234c4ea0edb148b23610b7c9
mgrone <226090022+mgrone@users.noreply.github.com>
2026-02-07 17:26:57 +0100

    script updated for the new cli

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-rtl_sdr -g 0.0 -f 1090000000 -s 2400000 - | ./build/stream1090 | socat -u - TCP4:localhost:30001 
+rtl_sdr -g 0.0 -f 1090000000 -s 2400000 - | ./build/stream1090 -s 2.4 -u 8 | socat -u - TCP4:localhost:30001 
 
 
