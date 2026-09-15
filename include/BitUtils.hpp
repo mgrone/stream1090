567dd980a58ed55384054e1a6bcb9344cab07aef
mgrone <226090022+mgrone@users.noreply.github.com>
2026-09-15 17:44:52 +0200

    Small fix for systems that do not support __uint128_t

--- include/BitUtils.hpp
+++ include/BitUtils.hpp
@@ -34,12 +34,31 @@ namespace BitUtils {
         constexpr uint64_t width = hi - lo + 1;
         static_assert(sizeof(ResType) * 8 >= width);
 
+// C++20 conditional block: Checks if the target platform natively supports 128-bit integers
+#if defined(__SIZEOF_INT128__)
+        // take the easy road
         __uint128_t x = ( (__uint128_t)high << 64 ) | low;
-
         constexpr uint64_t mask = (width == 64) ? ~uint64_t{0} : ((uint64_t{1} << width) - 1);
-
         return ResType((x >> lo) & mask);
+#else
+        // well then the long road
+        constexpr uint64_t mask = (width == 64) ? ~uint64_t{0} : ((uint64_t{1} << width) - 1);
+        uint64_t result = 0;
+
+        if constexpr (lo >= 64) {
+            result = high >> (lo - 64);
+        } 
+        else if constexpr (hi < 64) {
+            result = low >> lo;
+        } 
+        else {
+            constexpr std::size_t low_bits_count = 64 - lo;
+            result = (low >> lo) | (high << low_bits_count);
+        }
+        return ResType(result & mask);
+    #endif
     }
+
 
     template<typename ResType, std::size_t hi, std::size_t lo>
     constexpr ResType getBits(const Bits128& bits) noexcept {
